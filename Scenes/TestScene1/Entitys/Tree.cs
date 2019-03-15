68f68d07999ddd0cb3339807346da13c9a4f8cc6
Skyteck <sky.armada@gmail.com>
2019-03-15 15:12:41 -0500

    starting tree

---
+++ Scenes/TestScene1/Entitys/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECSTEST.Entitys;
+
+namespace ECSTEST.Scenes.TestScene1.Entitys
+{
+    class Tree : Entity
+    {
+        public Tree()
+        {
+
+        }
+    }
+}
